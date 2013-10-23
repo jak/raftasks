8f07e3ac7bf8b4da7263b3c7f285d2977980d809
Rafael Ruiz <rafaelruizmorales@gmail.com>
2013-10-23 23:11:32 +0100

    Update app.rb
    I have changed the code...

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'bundler/setup'
 require 'sinatra'
 
-get '/' do
-    tasks = []
-    (1..3).each { |i| tasks << "test task #{i}" }
+tasks = []
+(1..3).each { |i| tasks << "test task #{i}" }
 
+get '/' do
     tasks.join ', '
+end
+
+post '/add' do
+    unless params[:text].empty?
+        tasks << params[:text]
+        "Added: #{params[:text]}"
+    end
 end
